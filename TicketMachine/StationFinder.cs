197b407eeadd697e09dd3e830c9f729af359c333
willcurry <watercrafthd@gmail.com>
2018-10-07 18:03:34 +0100

    Initial commit

---
+++ TicketMachine/StationFinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMachine
+{
+    public class StationFinder
+    {
+        public String GetSuggestions(string userInput)
+        {
+            return "Dartford";
+        }
+    }
+}
---
+++ TicketMachineTests/StationFinderTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketMachine;
+
+namespace TicketMachineTests
+{
+    [TestFixture]
+    public class StationFinderTests
+    {
+        [Test]
+        public void FindsDartford()
+        {
+            StationFinder stationFinder = new StationFinder();
+            Assert.AreEqual(stationFinder.GetSuggestions("D"), "Dartford");
+        }
+    }
+}
